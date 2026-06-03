a10fd8ea42855ceb3da1181d42d577e38d2a6781
Tengz <redcraftrus@gmail.com>
2026-06-03 18:13:59 +0300

    chore: update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+module "api_gateway" {
+  source = "./modules/api_gateway"
+  healthcheck_lambda_invoke_arn = module.lambda.healthcheck_lambda_invoke_arn
+  environment = var.environment
+}
 
+module "iam" {
+  source = "./modules/iam"
+  environment = var.environment
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+  healthcheck_lambda_iam_role_arn = module.iam.healthcheck_lambda_iam_role_arn
+  environment = var.environment
+  log_level = var.log_level
+}
