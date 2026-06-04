b880499fe1ec5257b2262e8ad5a4122f22c47ad2
Tengz <redcraftrus@gmail.com>
2026-06-04 11:48:17 +0300

    fix: add billing mode for db

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,6 +1,7 @@
 resource "aws_dynamodb_table" "requests_db" {
   hash_key = "request_id"
   name     = "${var.environment}-requests-db"
+  billing_mode = "PAY_PER_REQUEST"
   attribute {
     name = "request_id"
     type = "S"
